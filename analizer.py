1b2d51b51478a58c7d5096c936f4b50a769856e0
Jorge <jorgejapao00@gmail.com>
2024-06-05 20:34:00 -0300

    Function to verify most sold product added

--- analizer.py
+++ analizer.py
@@ -1,3 +1,18 @@
 import pandas as pd
 
 dados_vendas = pd.read_csv('dados.csv')
+df = dados_vendas
+
+
+def mais_vendidos(mes_desejado):
+    df['Data'] = pd.to_datetime(df['Data'], format='%d/%m/%Y')
+
+    df_mes = df[(df['Data'].dt.month == mes_desejado)]
+
+    produtos_vendidos = df_mes.groupby('Produto')['Quantidade'].sum().reset_index()
+    produtos_vendidos = produtos_vendidos.sort_values(by='Quantidade', ascending=False)
+
+    print(f'Produtos mais vendidos - Mês {mes_desejado}')
+    print(produtos_vendidos)
+
+mais_vendidos(mes_desejado=4)
