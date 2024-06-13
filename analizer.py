1eb56206fdea3a8d4de2fd6e573ac134001c0e41
Jorge <jorgejapao00@gmail.com>
2024-06-13 19:52:19 -0300

    Payment method analysis added

--- analizer.py
+++ analizer.py
@@ -39,8 +39,23 @@ def media_vendas():
 
     print("Média de vendas por mês: R$ {:.2f}".format(media_vendas_por_mes))
 
+def metodo_pagamento_vendas(mes):
+    if mes:
+        df['Ano Mes'] = df['Data'].dt.to_period('M')    
+        pagamentos_por_metodo = df.groupby(['Ano Mes', 'Forma Pagamento']).size().reset_index(name='Quantidade')
+    
+    else:
+        pagamentos_por_metodo = df['Forma Pagamento'].value_counts().reset_index()
+        pagamentos_por_metodo.columns = ['Forma Pagamento', 'Quantidade']
+
+    print("Quantidade de vendas por método de pagamento:")
+    print(pagamentos_por_metodo)
+
 def run():
     mais_vendidos(mes_desejado=4)
     menos_vendidos(mes_desejado=2)
     meses(ascending=False)
     media_vendas()
+    metodo_pagamento_vendas(mes=True)
+
+run()
