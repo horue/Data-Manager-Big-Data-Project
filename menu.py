53b102761a2ac7c2796fddc85609cd6616cd4088
Jorge <jorgejapao00@gmail.com>
2024-06-13 17:05:45 -0300

    All menus updated to cal the function

--- menu.py
+++ menu.py
@@ -4,94 +4,89 @@ import os
 from analizer import *
 
 def principal_menu():
-    option=input("> ")
-    if option == ("1"):
-        print(1)
-    elif option == ('2'):
-        analisar()
-    elif option ==  ('3'):
-        ajuda()
-    elif option == ("4"):
+    option = input("> ")
+    if option == "1":
         sys.exit()
+    elif option == "2":
+        analisar()
+    elif option == "3":
+        exportar_dados()
+    elif option == "4":
+        ajuda()
     else:
         print("Por favor, entre um comando válido.")
         principal_menu()
 
 def analisar_menu():
-    option=input('> ')
-    if option == ('1'):
+    option = input('> ')
+    if option == '1':
         media_vendas()
-    elif option == ('2'):
+    elif option == '2':
         analisar_vendas()
-    elif option == ('3'):
-        meses()
-    elif option == ('4'):
-        principal
-    elif option == ('5'):
-        sys.exit
+    elif option == '3':
+        principal()
+    elif option == '4':
+        sys.exit()
     else:
         print('Por favor, entre um comando válido.')
         analisar_menu()
-        
+
 def analisar_vendas_menu():
-    option=input('> ')
-    if option == ('1'):
+    option = input('> ')
+    if option == '1':
         print('Produtos mais vendidos.')
-        mais_vendidos(mes_desejado=4)
-    elif option == ('2'):
+        mes = int(input('Digite o mês desejado (1-12): '))
+        mais_vendidos(mes_desejado=mes)
+    elif option == '2':
         print('Produtos menos vendidos.')
-    elif option == ('3'):
+        mes = int(input('Digite o mês desejado (1-12): '))
+        menos_vendidos(mes_desejado=mes)
+    elif option == '3':
         meses(ascending=False)
-    elif option == ('4'):
-        principal
-    elif option == ('5'):
-        sys.exit
+    elif option == '4':
+        analisar()
+    elif option == '5':
+        sys.exit()
     else:
         print('Por favor, entre um comando válido.')
         analisar_vendas_menu()
 
 def ajuda_menu():
-    option=input("> ")
-    if option.lower() ==  ('mais'):
+    option = input("> ")
+    if option.lower() == 'mais':
         wb.open('https://github.com/horue')
         principal()
-    if option.lower() == (''):
+    elif option.lower() == '':
         principal()
-    while option.lower() not in ['']:
-        print('Por favor, entre um comando válido. ')
-        option=input("> ")
-        return ajuda_menu()
+    else:
+        print('Por favor, entre um comando válido.')
+        ajuda_menu()
 
-    
 def principal():
-    os.system('cls')
+    os.system('cls' if os.name == 'nt' else 'clear')
     print("#" * 35)
     print('# Data Manager - Tela Inicial #')
     print("#" * 35)
     print('1 — Sair')
     print('2 — Analisar dados')
     print('3 — Exportar dados')
-    print('3 — Ajuda')
-    print('4 — Sair')
+    print('4 — Ajuda')
     print('Feito por Jorge Magno e Matheus Binder.')
     principal_menu()
 
-
 def ajuda():
-    os.system('cls')
+    os.system('cls' if os.name == 'nt' else 'clear')
     print("#" * 35)
     print('# Data Manager - Tela de Ajuda #')
     print("#" * 35)
     print('— Use comandos para acessar, alterar ou apagar dados no banco de dados.')
-    print("— Caso ainda tenha alguma dúvida, use o comando 'Mais' para ser redirecionado à docmuentação do programa.")
+    print("— Caso ainda tenha alguma dúvida, use o comando 'Mais' para ser redirecionado à documentação do programa.")
     print("— Para voltar à tela anterior, aperte Enter.")
     print('Feito por Jorge Magno e Matheus Binder.')
     ajuda_menu()
-    return(principal)
-
 
 def analisar():
-    os.system('cls')
+    os.system('cls' if os.name == 'nt' else 'clear')
     print("#" * 35)
     print('# Data Manager - Tela de Análise #')
     print("#" * 35)
@@ -103,14 +98,22 @@ def analisar():
     analisar_menu()
 
 def analisar_vendas():
-    os.system('cls')
+    os.system('cls' if os.name == 'nt' else 'clear')
     print("#" * 35)
-    print('# Data Manager - Tela de Análise #')
+    print('# Data Manager - Tela de Análise de Vendas #')
     print("#" * 35)
     print('1 — Produtos mais vendidos')
-    print('2 — Analisar menos vendidos')
+    print('2 — Produtos menos vendidos')
     print('3 — Meses com mais vendas')
     print('4 — Voltar')
     print('5 — Sair')
     print('Feito por Jorge Magno e Matheus Binder.')
     analisar_vendas_menu()
+
+def exportar_dados():
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print("#" * 35)
+    print('# Data Manager - Tela de Exportação #')
+    print("#" * 35)
+    print("Funcionalidade de exportação ainda não implementada.")
+    principal()
