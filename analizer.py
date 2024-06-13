4bf08614a86aca5a6e7a0d70a757b985cff833e5
Jorge <jorgejapao00@gmail.com>
2024-06-13 19:57:55 -0300

    Export function now works

--- analizer.py
+++ analizer.py
@@ -13,6 +13,7 @@ def mais_vendidos(mes_desejado):
 
     print(f'Produtos mais vendidos - Mês {mes_desejado}')
     print(produtos_vendidos)
+    return produtos_vendidos
 
 def menos_vendidos(mes_desejado):
     df_mes = df[(df['Data'].dt.month == mes_desejado)]
@@ -22,6 +23,7 @@ def menos_vendidos(mes_desejado):
 
     print(f'Produtos menos vendidos - Mês {mes_desejado}')
     print(produtos_vendidos)
+    return produtos_vendidos
 
 def meses(ascending):
     df['Ano Mes'] = df['Data'].dt.to_period('M')
@@ -30,6 +32,7 @@ def meses(ascending):
 
     vendas_por_mes = vendas_por_mes.sort_values(by='Preço Total', ascending=ascending)
     print(vendas_por_mes)
+    return vendas_por_mes
 
 def media_vendas():
     df['Ano Mes'] = df['Data'].dt.to_period('M')
@@ -38,6 +41,7 @@ def media_vendas():
     media_vendas_por_mes = vendas_por_mes['Preço Total'].mean()
 
     print("Média de vendas por mês: R$ {:.2f}".format(media_vendas_por_mes))
+    return media_vendas_por_mes
 
 def metodo_pagamento_vendas(mes):
     if mes:
@@ -50,6 +54,7 @@ def metodo_pagamento_vendas(mes):
 
     print("Quantidade de vendas por método de pagamento:")
     print(pagamentos_por_metodo)
+    return pagamentos_por_metodo
 
 def run():
     mais_vendidos(mes_desejado=4)
@@ -57,5 +62,22 @@ def run():
     meses(ascending=False)
     media_vendas()
     metodo_pagamento_vendas(mes=True)
+
+def exportar():
+    mais_vendidos_df = mais_vendidos(mes_desejado=4)
+    menos_vendidos_df = menos_vendidos(mes_desejado=2)
+    meses_df = meses(ascending=False)
+    media_vendas_valor = media_vendas()
+    metodo_pagamento_vendas_df = metodo_pagamento_vendas(mes=True)
+
+    mais_vendidos_df.to_csv('mais_vendidos.csv', index=False)
+    menos_vendidos_df.to_csv('menos_vendidos.csv', index=False)
+    meses_df.to_csv('vendas_por_mes.csv', index=False)
+    
+    media_vendas_df = pd.DataFrame({'Média de Vendas por Mês': [media_vendas_valor]})
+    media_vendas_df.to_csv('media_vendas.csv', index=False)
+    
+    metodo_pagamento_vendas_df.to_csv('metodo_pagamento_vendas.csv', index=False)
+
 
 run()
